397442ef06aee9e42956af39efd893f0004cabb7
kageru <kageru@encode.moe>
2020-06-02 11:12:26 +0200

    misc updates

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -3,7 +3,8 @@ alias qaac='wine ~/.wine/drive_c/qaac/qaac.exe'
 alias qaac_release=/mnt/hephaestos/qaac/qaac91
 alias define='python /home/kageru/programming/mwebster/mwebster.py'
 alias nanaone-stream='mpv rtmp://live1.brb.re/live/nanaone'
-alias ls='exa'
+alias ls=exa
+alias du=dua
 alias up='sudo pacman -Syu'
 alias in='sudo pacman -S'
 alias feg=feh # don’t ask
@@ -31,17 +32,27 @@ alias xo='xdg-open'
 alias resetgamma='xrandr --output DVI-D-0 --gamma 1:1:1 --output HDMI-1 --gamma 1:1:1 --output HDMI-0 --gamma 1:1:1 --output DP-0 --gamma 1:1:1'
 alias texbuild='pdflatex -interaction=nonstopmode'
 alias weather='curl wttr.in/hamburg -s | head -n 7 -'
-alias weathertoday='curl wttr.in/karlsruhe -s | head -n 17 -'
+alias weathertoday='curl wttr.in/hamburg -s | head -n 17 -'
 alias playdir='for f in ./*; do; mpv $f; done;'
 alias mntusb='sudo mount /dev/sdi1 ~/usb'
 alias mntphone='simple-mtpfs --device 1 ~/usb'
 alias umubs='sudo umount ~/usb'
 alias gst='git status'
 alias gco='git checkout'
+alias gs='git switch'
+alias gsc='git switch -c'
+alias gr='git restore'
 alias blps='~/.steam/steam/steamapps/common/BorderlandsPreSequel/BorderlandsPreSequel'
-alias dir2flac='for f in *.wav; do; ffmpeg -i "$f" -compression_level 12 "`basename "$f" .wav`.flac"; done;'
+alias dir2flac='for f in *.wav; do
+    ffmpeg -i "$f" -compression_level 12 "`basename "$f" .wav`.flac"
+done
+for f in *.WAV; do
+    ffmpeg -i "$f" -compression_level 12 "`basename "$f" .WAV`.flac"
+done;'
 alias vpnon='sudo systemctl start openvpn-client@client.service'
 alias vpnoff='sudo systemctl stop openvpn-client@client.service'
+alias tunnel='vpnoff && vpnon'
+alias getip='curl https://canihazip.com/s'
 
 alias config='/usr/bin/git --git-dir=/home/kageru/dotfiles/ --work-tree=/home/kageru'
 alias -g ...='../../'           #cd ...
